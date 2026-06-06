10378361ecc1f30357e216e4f295284519ffd9ab
suhanimakkarbsc2029-dev <suhani.makkar_bsc2029@tetr.com>
2026-06-06 18:30:35 +0530

    files_added

---
+++ worksheet 2/experiment1.py
@@ -0,0 +1,6 @@
+#Start with x = 1. Keep doubling it. Print every value until it becomes greater than 1000. Before coding - How many values do you think will appear?
+x = 1
+while x <= 1000:
+    print(x)
+    x *= 2   
+"it prints 10 values"
